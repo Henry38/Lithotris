ec9ae8cbcb170e9e5024bad41a9f54c7ae20d48b
Henry <lefevrehenry@hotmail.fr>
2019-03-16 12:12:30 +0100

    first try fallingObject

--- scripts/FallingObject.gd
+++ scripts/FallingObject.gd
@@ -1,12 +1,64 @@
 var _cellId
 var _cellPositions
 
-func _init(cellId, cellPOsitions):
+var x
+var y
+var width
+var height
+
+var matrix
+
+func _init(cellId, cellPositions):
 	self._cellId = cellId
-	self._cellPositions = cellPOsitions
+	self._cellPositions = cellPositions
+	self.x = 0
+	self.y = 0
 	
+	var min_x = cellPositions[0].x
+	var min_y = cellPositions[0].y
+	var max_x = cellPositions[0].x
+	var max_y = cellPositions[0].y
+	
+	for p in cellPositions:
+		if p.x < min_x:
+			min_x = p.x
+		if p.x > max_x:
+			max_x = p.x
+		if p.y < min_y:
+			min_y = p.y
+		if p.y > max_y:
+			max_y = p.y
+			
+		self.width = (max_x - min_x) + 1
+		self.height = (max_y - min_y) + 1
+
+	self.matrix = []
+	
+	for x in range(0,self.width):
+		self.matrix.append([])
+		for y in range(0,self.height):
+			self.matrix[x].append(-1)
+			
+	for p in cellPositions:
+		var x = p.x
+		var y = p.y
+		self.matrix[x][y] = cellId
+				
 func cell_id():
 	return _cellId
 	
-func cell_positions():
-	return _cellPositions
+func get_cell(x, y):
+	if x < 0 or x >= self.width or y < 0 or y >= self.height:
+		return -1
+		
+	return self.matrix[x][y]
+
+func cell_position():
+	return Vector2(x, y)
+
+func rotate_right():
+	pass
+		
+func rotate_left():
+	pass
+	
--- scripts/TileMap.gd
+++ scripts/TileMap.gd
@@ -1,15 +1,108 @@
 extends TileMap
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var FallingObject = preload("res://scripts/FallingObject.gd")
+
+# Main variables
+var create_new_block = true
+var current_block = null
+var blocks = []
+
+var pos = [Vector2(0,0), Vector2(1,0), Vector2(0,1), Vector2(2,0)]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.set_cell(10, 10, 1)
-	var c = self.get_cell(6, 6)
-	print(c)
+	# Set Timer connexion
+	$timer.connect("timeout", self, "trigger")
+
+func trigger():
+	# Creer nouvelle piece ?
+	if create_new_block:
+		create_new_block = false
+		createNewBlock()
+
+	# Si la  piece doit s'arreter
+	elif checkCollisionBlock():
+		create_new_block = true
+		current_block = null
+
+	# Sinon dscendre la piece	
+	else:
+		clearBlock()
+		moveBlock()
+
+	displayBlock()
+
+func createNewBlock():
+	var new_block = FallingObject.new(1, pos)
+	new_block.x = 10
+	new_block.y = 10
+	#var new_block = null #FallingObject.new()	# TODO + setter la position
+	current_block = new_block
+	blocks.append(new_block)
+
+# Return true if the current block touch the ground else false
+func checkCollisionBlock():
+	if current_block == null:
+		return
+		
+	var collision = false
+	var keep = []
+	var pos = current_block.cell_position()
+	var w = current_block.width
+	var h = current_block.height
+
+	for y in range(0,h):
+		for x in range(0,w):
+			var id = current_block.get_cell(x,y)
+			var id_below = current_block.get_cell(x,y+1)
+
+			if id != -1 and id_below == -1:
+				keep.append(Vector2(x,y))
+
+	for c in keep:
+		var x = pos.x + c.x
+		var y = pos.y + c.y + 1
+		var id = self.get_cell(x,y)
+		if id != -1:
+			collision = true
+			break
+
+	return collision
+
+func moveBlock():
+	if current_block == null:
+		return
+		
+	current_block.y += 1
+
+func clearBlock():
+	if current_block == null:
+		return
+
+	var pos = current_block.cell_position()
+	var w = current_block.width
+	var h = current_block.height
+
+	for y in range(0,h):
+		for x in range(0,w):
+			var id = current_block.get_cell(x,y)
+			if id != -1:
+				self.set_cell(pos.x + x, pos.y + y, -1)
+
+func displayBlock():
+	if current_block == null:
+		return
+
+	var pos = current_block.cell_position()
+	var w = current_block.width
+	var h = current_block.height
+
+	for y in range(0,h):
+		for x in range(0,w):
+			var id = current_block.get_cell(x,y)
+			if id != -1:
+				self.set_cell(pos.x + x, pos.y + y, id)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	pass
