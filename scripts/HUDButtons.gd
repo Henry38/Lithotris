23a6069ff8cd990873cea30cb55bda753181f77c
Pupier-Valade Florentin <gamer.florentin@gmail.com>
2019-03-17 09:46:30 +0100

    change menu animation and more
    commit

--- scripts/HUDButtons.gd
+++ scripts/HUDButtons.gd
@@ -6,6 +6,12 @@ export (bool) var show_pause_button = true
 func _ready():
 	if not show_pause_button:
 		$PauseButton.hide()
+		$SoundButton.connect("pressed", self, "set_mute_state")
+		
+func set_mute_state():
+	var ismuted = $SoundButton.pressed
+	for i in range (AudioServer.get_bus_count()):
+		AudioServer.set_bus_mute(i, ismuted)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
