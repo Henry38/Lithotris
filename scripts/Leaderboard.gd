9c613fa65a55dd85a1db920d1efa9b09a9c2f08d
Pupier-Valade Florentin <gamer.florentin@gmail.com>
2019-03-17 03:05:38 +0100

    Avant coucher
    commit

--- scripts/Leaderboard.gd
+++ scripts/Leaderboard.gd
@@ -4,7 +4,7 @@ extends Node
 # var a = 2
 # var b = "text"
 var lead_bdd =  "Wait"
-var lead = []
+var lead = [["Florentin", 12], ["Azarias", 43], ["Henri", 32]]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
