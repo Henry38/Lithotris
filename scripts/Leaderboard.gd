4345183da7d59608d49d87d9c651cd8be1702ef5
Pupier-Valade Florentin <gamer.florentin@gmail.com>
2019-03-17 11:40:57 +0100

    V1 leaderboard sort array

--- scripts/Leaderboard.gd
+++ scripts/Leaderboard.gd
@@ -9,6 +9,11 @@ var lead = [["Florentin", 12], ["Azarias", 43], ["Henri", 32]]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$BackButton.connect("pressed", self, "backb")
+	lead.sort_custom(self, "customComparison")
+	print (lead)
 	
 func backb():
 	Transition.fade_to("res://scenes/Menu.tscn")
+	
+func customComparison(a, b):
+        return a[1] < b[1]
