08fec8e3d6035ac17d991df4809c65f796c7d00b
Azarias Boutin <azarias.boutin@gmail.com>
2019-03-17 11:11:33 +0100

    Added pause popup
    and binding in hudbuttons

--- scripts/HUDButtons.gd
+++ scripts/HUDButtons.gd
@@ -7,6 +7,11 @@ func _ready():
 	if not show_pause_button:
 		$PauseButton.hide()
 		$SoundButton.connect("pressed", self, "set_mute_state")
+	else:
+		$PauseButton.connect("pressed", self, "pause_popup")
+		
+func pause_popup():
+	Popups.get_node("PauseDialog").popup_centered_minsize()
 		
 func set_mute_state():
 	var ismuted = $SoundButton.pressed
