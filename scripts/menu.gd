50e914347f7f361673762bc1e0705666b737a31f
Azarias Boutin <azarias.boutin@gmail.com>
2019-05-27 10:06:44 +0200

    Fixed #21

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -14,4 +14,13 @@ func _ready():
 
 func play():
 	MusicMenu.get_node("Music").stop()
+	var config = ConfigFile.new()
+	var err = config.load("user://settings.cfg")
+	if err == OK:
+		var did_tuto = config.get_value("config","did_tuto", false)
+		if did_tuto:
+			Transition.fade_to("res://scenes/Gameplay.tscn")
+			return
+	config.set_value("config", "did_tuto", true)
+	config.save("user://settings.cfg")
 	Transition.fade_to("res://scenes/Tutorial.tscn")
