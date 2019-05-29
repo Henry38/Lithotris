ab642d2e58d714bf1230b58154e48f40ddebbd13
Azarias Boutin <azarias.boutin@gmail.com>
2019-05-29 11:51:59 +0200

    Animated tiles !

--- scripts/global.gd
+++ scripts/global.gd
@@ -4,12 +4,12 @@ const EMPTY_TILE               = -1
 const BACKGROUND_TILE          = 0
 const CONDUCTOR_TILE           = 1
 const ISOLATOR_TILE            = 2
-const STARTING_TILE            = 3
-const ENDING_TILE              = 4
-const WALL_TILE                = 5
-const RESIN_TILE               = 6
-const RESIN_CLICKED_TILE       = 7
-const LIGHTENED_CONDUCTOR_TILE = 8
+const LIGHTENED_CONDUCTOR_TILE = 3
+const STARTING_TILE            = 4
+const ENDING_TILE              = 5
+const RESIN_CLICKED_TILE       = 6
+const RESIN_TILE               = 7
+const WALL_TILE                = 8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
